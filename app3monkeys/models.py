64dc3da0250e4b6d567646744c8b00a2daf44d9c
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-06-16 16:22:08 +0530

    signup

--- app3monkeys/models.py
+++ app3monkeys/models.py
@@ -61,8 +61,7 @@ class Booknow(models.Model):
 
     def __str__(self):
         return f"Booking by {self.full_name}"
-from django.db import models
-
+    
 class Activity(models.Model):
     title = models.CharField(max_length=255)
     type = models.CharField(max_length=100)
