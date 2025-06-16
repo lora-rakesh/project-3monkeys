225b01d41c36a8671f1a91e63e49f5387cd25390
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-06-16 16:17:47 +0530

    change login

--- app3monkeys/models.py
+++ app3monkeys/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class LoginEntry(models.Model):
     email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.email}"
     
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
