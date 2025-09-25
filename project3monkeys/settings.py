a56536d1cd27ee3e781395bb6e305bcb4b6424a6
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-09-25 12:38:38 +0530

    ok

--- project3monkeys/settings.py
+++ project3monkeys/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL ='/media/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
